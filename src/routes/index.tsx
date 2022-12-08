bbfc538cd821aff94efb8ca87f00e1d22c089a4e
yurisich <andrew@yurisich.com>
2022-12-07 23:34:07 -0500

    Insert color theme toggle in Nav

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,24 @@
 import { Title } from "solid-start";
-import { Box, Flex } from "@hope-ui/core";
+import { Box, Button, Flex, Heading, useColorMode } from "@hope-ui/core";
 
 import Nav from "~/components/Nav";
 
 export default function Home() {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
     <main>
-      <Box mx={12} my={10}>
+      <Box mx={8} my={4}>
         <Flex justifyContent="end">
-          <Nav />
+          <Nav title="Menu">
+            <Button onClick={toggleColorMode}>
+              Toggle { colorMode() === "light" ? "dark" : "light" } mode
+            </Button>
+          </Nav>
         </Flex>
       </Box>
       <Title>Foxcliff Chess Club</Title>
-      <h1>Foxcliff Chess Club</h1>
+      <Heading size="5xl">Foxcliff Chess Club</Heading>
     </main>
   );
 }
