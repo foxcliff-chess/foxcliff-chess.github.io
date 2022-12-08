2fd0d29638508725a7013c7b4cb2988a623eeddc
yurisich <andrew@yurisich.com>
2022-12-07 23:33:29 -0500

    Isolate Nav component boundaries

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,24 +1,31 @@
-import { createSignal } from "solid-js";
-import { IconButton, Drawer, HStack } from "@hope-ui/core";
+import { createSignal, ParentComponent, ParentProps } from "solid-js";
+import { Drawer, HStack, Icon, IconButton, IconProps } from "@hope-ui/core";
 
-export default function Nav() {
+function HambugerIcon(props: IconProps) {
+  return (
+    <Icon viewBox="0 0 100 80" { ...props }>
+      <rect width="100" height="20"></rect>
+      <rect y="30" width="100" height="20"></rect>
+      <rect y="60" width="100" height="20"></rect>
+    </Icon>
+  );
+}
+
+export default function Nav(props: ParentProps) {
     const [isOpen, setIsOpen] = createSignal(false);
     return (
       <>
-      <IconButton size="lg" border="none" aria-label="Navigation" onClick={() => setIsOpen(true)}>
-        <svg viewBox="0 0 100 80" width="40" height="40">
-          <rect width="100" height="20"></rect>
-          <rect y="30" width="100" height="20"></rect>
-          <rect y="60" width="100" height="20"></rect>
-        </svg>
+      <IconButton as="a" aria-label="Menu" size="lg" variant="plain" colorScheme="neutral" onClick={() => setIsOpen(true)}>
+        <HambugerIcon boxSize="14" />
         <Drawer isOpen={isOpen()} onClose={() => setIsOpen(false)}>
           <Drawer.Overlay />
           <Drawer.Content p={4}>
             <HStack justifyContent="space-between" mb={4}>
-              <Drawer.Heading fontWeight="semibold">Title</Drawer.Heading>
-              <Drawer.CloseButton />
+             <Drawer.Heading fontWeight="semibold">{props.title}</Drawer.Heading>
+             <Drawer.CloseButton />
             </HStack>
-            <p>The content of the Drawer.</p>
+
+            {props.children}
           </Drawer.Content>
         </Drawer>
       </IconButton>
