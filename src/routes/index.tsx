d9033daa42eca492224ed5e17e261c25e0ca4cfe
yurisich <andrew@yurisich.com>
2023-04-30 20:52:38 -0400

    Add April news update while it's still April.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 // import { createSignal } from "solid-js";
 import { Title } from "solid-start";
-import { Anchor, Box, Button, Flex, Text, VStack, useColorMode } from "@hope-ui/core";
+import { Anchor, Box, Button, Center, Flex, Image, Text, VStack, useColorMode } from "@hope-ui/core";
 
 import Nav from "~/components/Nav";
 import Hero from "~/components/Hero";
@@ -12,21 +12,38 @@ import navIsOpen from "~/signals/nav/isOpen";
 import NextMeeting from "~/components/NextMeeting";
 import NextMeetingCalendar from "~/components/NextMeetingCalendar";
 
+const linearGradientLight = `linear-gradient(var(--hope-colors-neutral-50), var(--hope-colors-neutral-300))`;
+const linearGradientDark = `linear-gradient(var(--hope-colors-neutral-900), var(--hope-colors-neutral-700))`;
 export default function Home() {
   const { colorMode, toggleColorMode } = useColorMode();
   const [isOpen, setIsOpen] = navIsOpen;
 
   return (
     <main>
-      <Box bg={`linear-gradient(var(--hope-colors-neutral-50), var(--hope-colors-neutral-300))`} _dark={{ bg: `linear-gradient(var(--hope-colors-neutral-900), var(--hope-colors-neutral-700))`}}>
+      <Box bg={linearGradientLight} _dark={{ bg: linearGradientDark}}>
         <Flex justifyContent="end">
           <Nav title="Menu" mx={8} my={4} py={2}>
             <VStack spacing={8}>
               <Button onClick={toggleColorMode}>
                 Toggle { colorMode() === "light" ? "dark" : "light" } mode
               </Button>
-              <Button variant="plain" onClick={() => setIsOpen(false)}>
+              <Button w="100%" variant="plain" onClick={() => {
+                setIsOpen(false);
+                document.querySelector('a[href="/#section-Club-News"]').scrollIntoView();
+              }}>
+                Club News
+              </Button>
+              <Button w="100%" variant="plain" onClick={() => {
+                setIsOpen(false);
+                document.querySelector('a[href="/#section-History"]').scrollIntoView();
+              }}>
                 History
+              </Button>
+              <Button w="100%" variant="plain" onClick={() => {
+                setIsOpen(false);
+                document.querySelector('a[href="/#section-Calendar"]').scrollIntoView();
+              }}>
+                Calendar
               </Button>
             </VStack>
           </Nav>
@@ -34,6 +51,60 @@ export default function Home() {
         <Title>Foxcliff Chess Club</Title>
         <Hero />
         <VStack spacing={8}>
+          <Section heading="Club News">
+            <Text m="2" size="xl">
+              Congratulations to Club members who had success in recent tournaments:
+            </Text>
+
+            <Text m="2" size="xl">
+              <ul>
+                <li>Jackson Matthews won his division in the regional finals.</li>
+                <li>Carter Dawson finished third in the junior open division</li>
+                <li>Kevin Huang finished 2 nd and Ryan Pilat finished in 7 th in the State individual Championships</li>
+                <li>Kevin Huang, Ryan Pilat, and Joel Young, and their 4th teammate, Tom, completed in the Indiana</li>
+              </ul>
+            </Text>
+
+            <Text m="2" size="xl">
+              Team Championships in Pittsboro, Indiana.
+              Their team called “Dark Horse & the Kingslayers” won 2.5 rounds out of 4.
+              They were the top team rated under 1500 (meaning they won a little money), and they tied for 3rd place overall!
+            </Text>
+
+            <Text m="2" size="xl">
+              Congratulations, gentlemen!
+            </Text>
+
+            <Text m="2" size="xl">
+              <ul>
+                <li>The Martinsville High School Chess A Team finished 4th in the State.</li>
+              </ul>
+            </Text>
+
+            <Text m="2" size="xl">
+              Many of our members volunteered to help run the Indiana Scholastic Championships held on March 18th at Martinsville High School.
+            </Text>
+
+            <Text m="2" size="xl">
+              Check out our new snazzy Foxcliff Chess Club T- Shirts. They are available in various sizes, and the prices range from $8.50 to $12.
+            </Text>
+
+            <Center>
+              <Image src="/i/tshirt/front.jpg"/>
+            </Center>
+            <Center>
+              <Image src="/i/tshirt/back.jpg"/>
+            </Center>
+
+            <Text m="2" size="xl">
+              Congratulations to Bill Pilat who has taken the reigns of the President of the Scholastic Chess of Indiana.
+            </Text>
+
+            <Text m="2" size="xl">
+              Three of our members will work to achieve the title of Club Tournament Director this year.
+            </Text>
+
+          </Section>
           <Section heading="History">
             <Text m="2" size="xl">
             In the month of February 2022, a resident of Foxcliff North Estates, Martinsville, Indiana had posted a notice on the Foxcliff general email for anyone who wanted to play a game of chess.
