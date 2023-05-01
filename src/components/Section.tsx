b35a1d1243b36cfde8fdc95bb01e7fe99d1bf087
yurisich <andrew@yurisich.com>
2023-04-30 20:52:38 -0400

    Add April news update while it's still April.

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,12 +1,18 @@
 import { ParentProps } from "solid-js";
-import { Box, Divider, Heading, VStack } from "@hope-ui/core";
+import { Anchor, Box, Divider, Heading, VStack } from "@hope-ui/core";
 
-type Props = {heading: string} & ParentProps
+type Props = {heading: string, slug?: string} & ParentProps
 export default function Section(props: Props) {
+  const slug = props.slug || props.heading.replace(/\W+/g, '-');
+
   return (
     <Box maxW={880}>
       <VStack spacingY={12} my={12}>
-        <Divider labelPlacement="center" thickness="3px"><Heading size="2xl">{props.heading}</Heading></Divider>
+        <Divider labelPlacement="center" thickness="3px">
+          <Anchor href={`/#section-${slug}`}>
+            <Heading size="2xl">{props.heading}</Heading>
+          </Anchor>
+        </Divider>
         <Box mx={4}>
           {props.children}
         </Box>
